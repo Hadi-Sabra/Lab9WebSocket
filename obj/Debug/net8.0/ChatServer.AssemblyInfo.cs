17b72d9e1a905ab9a955d29c7f61bcb55c5d5141
Hadi-Sabra <131852949+Hadi-Sabra@users.noreply.github.com>
2025-03-09 05:02:11 +0200

    Migrations Done

--- obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33390857f2318efcf5e1eb8ba8b96e3cae116e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967922da385cd66fc2ab519e82bd790fee645847")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
